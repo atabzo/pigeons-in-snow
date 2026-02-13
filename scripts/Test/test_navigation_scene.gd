a25b7ac942ed2d8e2547db33f9041f1be4ee1ebb
DiScarlet <antoniukdin@gmail.com>
2026-02-13 22:01:57 +0100

    Adapted code for n nodes. Optimized algorithm usage

--- scripts/Test/test_navigation_scene.gd
+++ scripts/Test/test_navigation_scene.gd
@@ -1,53 +1,75 @@
 extends Node2D
 
 #vars
-@onready var test_node_1: Node2D = $TestNode1
-@onready var test_node_2: Node2D = $TestNode2
+@onready var nodes: Array = $Waypoints.get_children()
+
 @onready var navigation_region_2d: NavigationRegion2D = $NavigationRegion2D
 @onready var drawer = $PathDrawer
-@onready var label: Label = $Label
+
+var last_positions: Array[Vector2] = []
+var map: RID
 
 #func
 func _ready():
 	await wait_for_navigation_ready()
-	await NavigationServer2D.map_changed
-	print("map_changed")
-	await get_tree().process_frame
+	map = get_world_2d().navigation_map
 
-	var map = get_world_2d().navigation_map
-
-	var path = NavigationServer2D.map_get_path(
-		map,
-		test_node_1.global_position,
-		test_node_2.global_position,
-		false
-	)
-	print("Map: " + str(map))
-	print("Path found: " + str(path))
-
-	print("Point A:", test_node_1.global_position)
-	print("Point B:", test_node_2.global_position)
-	
-	label.text = str(path)
-	drawer.set_path(path)
-
-func _process(delta):
+	cache_positions()
 	update_path()
 	
+func _process(delta):
+	if waypoints_changed():
+		update_path()
+		cache_positions()
+
+	
 func update_path():
-	var map = get_world_2d().navigation_map
-	
-	var path = NavigationServer2D.map_get_path(
-		map,
-		test_node_1.global_position,
-		test_node_2.global_position,
-		false
-	)
-	
+	var path = build_path_through_nodes()
 	drawer.set_path(path)
+
+
 	
+#get path for n nodes
+func build_path_through_nodes() -> PackedVector2Array:
+	var map = get_world_2d().navigation_map
+	var final_path: PackedVector2Array = PackedVector2Array()
+	
+	for i in range(nodes.size() - 1):
+		var segment = NavigationServer2D.map_get_path(
+			map,
+			nodes[i].global_position,
+			nodes[i + 1].global_position,
+			false
+		)
+		
+		if segment.is_empty():
+			continue
+		
+		# Avoid duplicating joint points
+		if final_path.size() > 0:
+			segment.remove_at(0)
+		
+		final_path.append_array(segment)
+	
+	return final_path
+
+#helpers
+#await for load
 func wait_for_navigation_ready():
 	var map = get_world_2d().navigation_map
 	
 	while NavigationServer2D.map_get_regions(map).is_empty():
 		await get_tree().process_frame
+	
+#check if location of the nodes was changed	
+func waypoints_changed() -> bool:
+	for i in range(nodes.size()):
+		if nodes[i].global_position != last_positions[i]:
+			return true
+	return false
+	
+#cache positions for each node
+func cache_positions():
+	last_positions.clear()
+	for node in nodes:
+		last_positions.append(node.global_position)
--- scripts/Test/test_node_1.gd
+++ scripts/Test/test_node_1.gd
@@ -1,5 +1,7 @@
 extends  Node2D
 
+@export var hor := true
+@export var ver := false
 @export var distance := 100.0
 @export var speed := 100.0
 
@@ -11,10 +13,20 @@ func _ready():
 
 
 func _process(delta):
-	global_position.x += direction * speed * delta
+	if hor:
+		global_position.x += direction * speed * delta
+		
+		# Check bounds
+		if global_position.x <= start_position.x - distance:
+			direction = 1
+		elif global_position.x >= start_position.x:
+			direction = -1
 	
-	# Check bounds
-	if global_position.x <= start_position.x - distance:
-		direction = 1
-	elif global_position.x >= start_position.x:
-		direction = -1
+	if ver:
+		global_position.y += direction * speed * delta
+		
+		# Check bounds
+		if global_position.y <= start_position.y - distance:
+			direction = 1
+		elif global_position.y >= start_position.y:
+			direction = -1
