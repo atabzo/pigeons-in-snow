72dfdad012d317ba95de17dcf54e965099a29a35
DiScarlet <antoniukdin@gmail.com>
2026-02-17 00:22:57 +0100

    Added intial setting of the scene + field of view

---
+++ scripts/Chapter2/Npc2/field_of_view.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var ROTATION_SPEED: float = 50.0
+@export var MAX_ROTATION: float = 30.0
+@export var START_LEFT_TO_RIGHT: bool = true
+
+var base_rotation: float = 0.0
+var offset: float = 0.0
+var direction: int = 1
+
+var frame_counter: int = 0
+var frame_interval: int = 5
+
+func _ready() -> void:
+	base_rotation = 0 if START_LEFT_TO_RIGHT else 180
+	rotation_degrees = base_rotation
+
+
+func _process(delta: float) -> void:
+	frame_counter += 1
+	if frame_counter < frame_interval:
+		return
+	frame_counter = 0
+
+	offset += ROTATION_SPEED * direction * delta
+
+	if offset > MAX_ROTATION:
+		offset = MAX_ROTATION
+		direction = -1
+	elif offset < -MAX_ROTATION:
+		offset = -MAX_ROTATION
+		direction = 1
+
+	rotation_degrees = base_rotation + offset
